940393e7e7b897470e80b1b1216143093b67e06e
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-05-20 22:04:27 +0100

    Created rough outline of change event listener

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//
+// Function to add 'change' event listener
+function addChangeEvent(element) {
+  const eleToAddEvent = document.getElementById(element);
+  eleToAddEvent.addEventListener("change", () => {
+    if (eleToAddEvent.checkValidity()) {
+      eleToAddEvent.classList.add("valid");
+      eleToAddEvent.classList.remove("invalid");
+    } else {
+      eleToAddEvent.classList.add("invalid");
+      eleToAddEvent.classList.remove("valid");
+    }
+  });
+}
+
+addChangeEvent();
